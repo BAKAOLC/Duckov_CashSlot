a7f03841a970f7d89e7ae9482a83549fc7204b10
OLC <utsumabo@ritsukage.com>
2025-10-24 07:45:38 +0800

    Add Harmony patch for InteractableBase to enhance item requirement checks

---
+++ Duckov_CashSlot/HarmonyPatches/InteractableBase_TryGetRequiredItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using HarmonyLib;
+using ItemStatsSystem;
+
+namespace Duckov_CashSlot.HarmonyPatches
+{
+    [HarmonyPatch(typeof(InteractableBase), nameof(InteractableBase.TryGetRequiredItem))]
+    // ReSharper disable once InconsistentNaming
+    internal class InteractableBase_TryGetRequiredItem
+    {
+        // ReSharper disable InconsistentNaming
+        private static void Postfix(InteractableBase __instance,
+                CharacterMainControl fromCharacter,
+                ref (bool hasItem, Item ItemInstance) __result,
+                bool ___requireItem
+            )
+            // ReSharper restore InconsistentNaming
+        {
+            if (__result.hasItem) return;
+            if (fromCharacter == null) return;
+
+            var registeredSlot = SlotManager.GetAllRegisteredSlotsInItem(fromCharacter.CharacterItem);
+            if (registeredSlot.Length == 0) return;
+
+            var slotItems = registeredSlot.Select(slot => slot.Content).Where(item => item != null).ToArray();
+            foreach (var item in slotItems)
+            {
+                if (item.TypeID == __instance.requireItemId)
+                {
+                    __result = (true, item);
+                    return;
+                }
+
+                if (item.Slots == null || item.Slots.Count <= 0) continue;
+                foreach (var slot in item.Slots)
+                {
+                    if (slot.Content == null || slot.Content.TypeID != __instance.requireItemId) continue;
+                    __result = (true, slot.Content);
+                    return;
+                }
+            }
+        }
+    }
+}

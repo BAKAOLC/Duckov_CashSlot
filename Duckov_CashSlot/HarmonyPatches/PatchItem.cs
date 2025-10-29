5699c8a0f97e7498ff421cdab4bde78f4c440874
OLC <utsumabo@ritsukage.com>
2025-10-29 16:37:32 +0800

    Add Harmony patch to override Item total weight calculation

---
+++ Duckov_CashSlot/HarmonyPatches/PatchItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using ItemStatsSystem;
+using UnityEngine;
+
+namespace Duckov_CashSlot.HarmonyPatches
+{
+    [HarmonyPatch]
+    internal class PatchItem
+    {
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.PropertyGetter(typeof(Item), nameof(Item.TotalWeight));
+        }
+
+        // ReSharper disable InconsistentNaming
+        private static void Postfix(Item __instance, ref float __result)
+            // ReSharper restore InconsistentNaming
+        {
+            if (__instance == null) return;
+            if (__instance.PluggedIntoSlot == null) return;
+            if (!SlotManager.IsRegisteredSlot(__instance.PluggedIntoSlot)) return;
+            if (!SlotManager.IsSlotForbidWeightCalculation(__instance.PluggedIntoSlot)) return;
+
+            __result = 0;
+        }
+    }
+}
